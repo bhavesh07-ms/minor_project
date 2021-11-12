0a60978b25813bc5ded51c1175305dae7b8c491c
harshsahu15 <94159866+harshsahu15@users.noreply.github.com>
2021-11-12 15:47:51 +0530

    Update index.js

--- minor_project/src/index.js
+++ minor_project/src/index.js
@@ -1,5 +1,29 @@
 const dropZone = document.querySelector(".drop-zone");
+const browseBtn = document.querySelector(".browseBtn");
+const fileInput = document.querySelector("#fileInput");
+
 dropZone.addEventListener("dragover", (e) => {
-    console.log("dragged");
-    dropZone.classList.add("dragged");
+    e.preventDefault();
+    if (!dropZone.classList.contains("dragged")) {
+        dropZone.classList.add("dragged");
+    }   
+});
+
+dropZone.addEventListener("dragleave", () => {
+    dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropZone.classList.remove("dragged");
+    const files = e.dataTransfer.files
+    console.log(files);
+    if (files.length) {
+        fileInput.files = files;
+    }
+ 
 })
+
+browseBtn.addEventListener("click", () => {
+    fileInput.click();
+});
