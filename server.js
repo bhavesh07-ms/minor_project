e0d6b943eb95915cfcae708c233de3c091fd892a
harshsahu15 <94159866+harshsahu15@users.noreply.github.com>
2021-11-19 15:26:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+//Routes-
+app.use('/api/files', require('./routes/files'));
+
 app.listen(PORT, () => {
     console.log(`Listening at ${PORT}`)
-})
+});
