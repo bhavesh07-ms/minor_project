f79bea00c71e8412c14b3e638cb79d94b6211670
bhavesh07-ms <76205228+bhavesh07-ms@users.noreply.github.com>
2021-11-20 17:05:33 +0530

    Add files via upload

--- routes/files.js
+++ routes/files.js
@@ -5,12 +5,11 @@ const File = require('../models/file');
 const { v4: uuid4 } = require('uuid');
 
 
-
 let storage = multer.diskStorage({
 	destination: (req, file, cb) => cb(null, 'uploads/'),
 	filename: (req, file, cb) => {
 		const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
-            cb(null, uniqueName);
+            cb(null, uniqueName);//date-9digituniquecode-.pdf/.jpg
 	},
 });
 
@@ -24,12 +23,14 @@ let upload = multer({
 
 router.post('/', (req, res) => {
 	//Validate Request
-		if(!req.file) {
-			return res.json({ error : 'All fields are required.'});
-		}
+	
 
 	//Store file
 		upload(req, res, async (err) => {
+            if(!req.file) {
+                return res.json({ error : 'All fields are required.'});
+            }
+
 			if(err) {
 				return res.status(500).send({ error: err.message })
 			}
@@ -40,9 +41,9 @@ router.post('/', (req, res) => {
 				path: req.file.path,
 				size: req.file.size
 			});
-
 			const response = await file.save();
-			return res.json({ file: `${process.env.APP_BASE_URL}`})
+			return res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}`})
+            //http://localhost:3000/files/uniqueid
 
 		});
 
