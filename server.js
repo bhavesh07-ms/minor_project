bd310dcfafc292bd2d2b7a8fde9f8a368f521628
harshsahu15 <94159866+harshsahu15@users.noreply.github.com>
2021-11-21 12:23:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(express.static('public'));
 
 app.use('/api/files',require('./routes/files'));
 
-app.use('/files',require('./routes/show'))
+app.use('/files',require('./routes/show'));
+
+app.use('/files/download', require('./routes/download'));
+
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`)
