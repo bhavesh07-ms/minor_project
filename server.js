e29077a2c3ee3c0ba9f97db9e647d59f1507ffea
harshsahu15 <94159866+harshsahu15@users.noreply.github.com>
2021-11-22 10:06:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+//Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+
+app.use(cors(corsOptions));
+
+
 
 //Template engine
 app.set('views',path.join(__dirname,'/views'));
@@ -13,12 +24,13 @@ app.set('view engine','ejs');
 
 app.use(express.static('public'));
 
+app.use(express.json());
+
 app.use('/api/files',require('./routes/files'));
 
-app.use('/files',require('./routes/show'));
+app.use('/files',require('./routes/show'))
 
-app.use('/files/download', require('./routes/download'));
-
+app.use('/files/download',require('./routes/download'))
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`)
